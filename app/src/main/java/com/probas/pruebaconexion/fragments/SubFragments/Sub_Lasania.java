61af25d4002e22ca2bddc8282cf47504dc7c6fa2
Carlex <packcharce@gmail.com>
2018-05-19 14:59:48 +0200

    S

--- app/src/main/java/com/probas/pruebaconexion/fragments/SubFragments/Sub_Lasania.java
+++ app/src/main/java/com/probas/pruebaconexion/fragments/SubFragments/Sub_Lasania.java
@@ -64,8 +64,7 @@ public class Sub_Lasania extends Fragment {
 
         final RecyclerView mRecyclerView = v.findViewById(R.id.rec_lasania_pedido);
 
-        // use this setting to improve performance if you know that changes
-        // in content do not change the layout size of the RecyclerView
+
         mRecyclerView.setHasFixedSize(true);
 
         // use a linear layout manager
